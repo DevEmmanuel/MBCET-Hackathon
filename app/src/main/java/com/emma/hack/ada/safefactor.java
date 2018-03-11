8168aafb6ea2b26a31d81dac4e57980df853411e
Andrew Emmanuel Lopez <devemmaa@yahoo.com>
2018-03-11 09:23:39 +0530

    First Commit

---
+++ app/src/main/java/com/emma/hack/ada/safefactor.java
@@ -0,0 +1,14 @@
+package com.emma.hack.ada;
+
+public class safefactor {
+
+    String safe;
+
+    public String getSafe() {
+        return safe;
+    }
+
+    public void setSafe(String safe) {
+        this.safe = safe;
+    }
+}
